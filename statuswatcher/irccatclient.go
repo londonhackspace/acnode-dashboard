cfee396c1dc21b6e57ae295d773a801dad8ac2cc
Michael Cullen <michael181189@gmail.com>
2021-11-15 04:32:56 +0100

    IRC Tweaks after looking at doorbot announcers
    I remember without the newline, it doesn't work properly

--- statuswatcher/irccatclient.go
+++ statuswatcher/irccatclient.go
@@ -18,12 +18,14 @@ func MakeIRCCatClient(server string, channel string) *IRCCatClient {
 }
 
 func (irccat *IRCCatClient) SendMessage(message string) {
-	conn, err := net.Dial("tcp", irccat.server)
-	if err != nil {
-		log.Err(err).Msg("Error connecting to IRCCat")
-		return
-	}
-	defer conn.Close()
+	go func() {
+		conn, err := net.Dial("tcp", irccat.server)
+		if err != nil {
+			log.Err(err).Msg("Error connecting to IRCCat")
+			return
+		}
+		defer conn.Close()
 
-	conn.Write([]byte(irccat.channel + " " + message))
+		conn.Write([]byte(irccat.channel + " " + message + "\n"))
+	}()
 }
