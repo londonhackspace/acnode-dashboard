4bcadff6807effa5e31df576f8db655529b81180
Michael Cullen <michael@michaelcullen.name>
2021-12-01 21:39:00 +0100

    StatusWatcher: Ignore time when calculating health of transient node
    Transient nodes come and go as the tool is used. This means that the node health cannot sensibly include time in its metrics, since this raises needless alerts when the tool is powered down and started up.
    
    This commit makes considering last seen time conditional on the node not being transient.

--- statuswatcher/statuswatcher_calculator.go
+++ statuswatcher/statuswatcher_calculator.go
@@ -19,55 +19,52 @@ func (sw *StatusWatcher) calculateNodeState(node *apitypes.ACNode) (int, []strin
 	lastSeenMQTT := time.Now().Sub(time.Unix(node.LastSeenMQTT, 0)).Seconds()
 	lastStarted := time.Now().Sub(time.Unix(node.LastStarted, 0)).Seconds()
 
-	if node.LastSeenMQTT > -1 || node.LastSeenAPI > -1 {
-		// if we're seeing neither MQTT or ACServer log entries,
-		// it's probably dead
-		if (lastSeenApi > 610 || node.LastSeenAPI == -1) &&
-			 (lastSeenMQTT > 610 || node.LastSeenMQTT == -1) {
-			healthHints = append(healthHints, "Has not been seen online in any form in over 10 minutes")
-			if node.IsTransient {
+	// Only consider time if the node isn't flagged as transient
+	if !node.IsTransient {
+		if node.LastSeenMQTT > -1 || node.LastSeenAPI > -1 {
+			// if we're seeing neither MQTT or ACServer log entries,
+			// it's probably dead
+			if (lastSeenApi > 610 || node.LastSeenAPI == -1) &&
+				(lastSeenMQTT > 610 || node.LastSeenMQTT == -1) {
+				healthHints = append(healthHints, "Has not been seen online in any form in over 10 minutes")
+
+				return STATE_BAD, healthHints
+			}
+
+			if node.LastSeenMQTT == -1 || lastSeenMQTT > 130 {
+				healthHints = append(healthHints, "Has not sent a message via MQTT in over 2 minutes")
+				health = STATE_WARN
+			}
+
+			// unrestricted doors don't check in nearly so often if they're not
+			// running firmware new enough to periodically revalidate the cache
+			if node.Type == "Door" {
+				if node.LastSeenAPI == -1 || lastSeenApi > ((3600*12)+10) {
+					healthHints = append(healthHints, "Has not contacted ACServer in over 12 hours")
+					health = STATE_WARN
+				}
+			} else {
+				if node.LastSeenAPI == -1 || lastSeenApi > 610 {
+					healthHints = append(healthHints, "Has not contacted ACServer in over 10 minutes")
+					health = STATE_WARN
+				}
+			}
+
+		} else {
+			// I'm not actually convinced we need this now, since I think all nodes have API and MQTT
+			// values separately, but since we're porting the TS logic...
+			if node.LastSeen == -1 {
+				healthHints = append(healthHints, "Has never been seen online")
 				return STATE_UNKNOWN, healthHints
 			}
-			return STATE_BAD, healthHints
-		}
 
-		if node.LastSeenMQTT == -1 || lastSeenMQTT > 130 {
-			healthHints = append(healthHints, "Has not sent a message via MQTT in over 2 minutes")
-			health = STATE_WARN
-		}
-
-		// unrestricted doors don't check in nearly so often if they're not
-		// running firmware new enough to periodically revalidate the cache
-		if node.Type == "Door" {
-			if node.LastSeenAPI == -1 || lastSeenApi > ((3600*12) + 10) {
-				healthHints = append(healthHints, "Has not contacted ACServer in over 12 hours")
-				health = STATE_WARN
-			}
-		} else {
-			if node.LastSeenAPI == -1 || lastSeenApi > 610 {
-				healthHints = append(healthHints, "Has not contacted ACServer in over 10 minutes")
-				health = STATE_WARN
-			}
-		}
-
-	} else {
-		// I'm not actually convinced we need this now, since I think all nodes have API and MQTT
-		// values separately, but since we're porting the TS logic...
-		if node.LastSeen == -1 {
-			healthHints = append(healthHints, "Has never been seen online")
-			return STATE_UNKNOWN, healthHints
-		}
-
-		if lastSeen > 610 {
-			healthHints = append(healthHints, "Has not been seen online in over 10 minutes")
-			if node.IsTransient {
-				health = STATE_UNKNOWN
-			} else {
+			if lastSeen > 610 {
+				healthHints = append(healthHints, "Has not been seen online in over 10 minutes")
 				health = STATE_BAD
+			} else if lastSeen > 70 {
+				healthHints = append(healthHints, "Has not been seen online in over a minute")
+				return STATE_WARN, healthHints
 			}
-		} else if lastSeen > 70 {
-			healthHints = append(healthHints, "Has not been seen online in over a minute")
-			return STATE_WARN, healthHints;
 		}
 	}
 
