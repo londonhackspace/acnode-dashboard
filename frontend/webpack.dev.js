d3016e72ba451dc8c44c7198b2be81d8a66bf414
Michael Cullen <michael@michaelcullen.name>
2021-12-07 21:18:14 +0100

    Update JS Stuff
    Pulling in some updates to the npm packages

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -6,9 +6,18 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: path.join(__dirname, "static"),
+        client: {
+            webSocketURL: {
+                pathname: "/ws_debug"
+            }
+        },
+        devMiddleware: {
+            publicPath: "http://localhost:3000/",
+        },
+        static: {
+            directory: path.join(__dirname, "static"),
+        },
         port: 3000,
-        publicPath: "http://localhost:3000/",
         hot: true,
         proxy: {
             '/api': 'http://localhost:8080',
